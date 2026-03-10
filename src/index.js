1c58e79cfca8faa4fce51f04d5fcef0904eb5f1f
zemlyansky <anton.zemlyansky@gmail.com>
2026-03-10 05:42:20 +0000

    refactor: wrap TsetlinModel with createModelClass for unified task detection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const { loadTsetlin, getWasm } = require('./wasm.js')
-const { TsetlinModel } = require('./model.js')
+const { TsetlinModel: TsetlinModelImpl } = require('./model.js')
+const { createModelClass } = require('@wlearn/core')
+
+const TsetlinModel = createModelClass(TsetlinModelImpl, TsetlinModelImpl, { name: 'TsetlinModel', load: loadTsetlin })
 
 // Convenience: create, fit, return fitted model
 async function train(params, X, y) {
   const model = await TsetlinModel.create(params)
-  model.fit(X, y)
+  await model.fit(X, y)
   return model
 }
 
